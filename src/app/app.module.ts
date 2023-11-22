b6fb5da70f8d67b11f35ca98c9a040a1c5aa6fc7
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-22 21:58:54 +0100

    feat: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { SendComponent } from './display/send/send.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    SendComponent
   ],
   imports: [
     BrowserModule,
