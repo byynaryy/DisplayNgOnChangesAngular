759dd6269937e8c99be47e9cecad8b14eca720f6
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-22 22:59:22 +0100

    feat(kinder.component.ts): add Input decorator

--- src/app/eltern/kinder/kinder.component.ts
+++ src/app/eltern/kinder/kinder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kinder',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class KinderComponent {
 
+    @Input() item: string = ''; // decorate the property with @Input()
 }
